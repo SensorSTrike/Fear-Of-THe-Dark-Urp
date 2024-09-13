33412bf3828ce64df2e1e8b1c1ebbf4f98bcc67c
zzKermaperuna <artti.kahilainen@gmail.com>
2024-09-13 11:22:31 +0300

    Testi Push
    Lisäsin turhan skriptin testimielessä, poistan sen seuraavaksi

---
+++ Fear-Of-The-Dark/Assets/Scripts/TestiSkriptiPushAndPull.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestiSkriptiPushAndPull : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // T‰tt‰d‰‰ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
